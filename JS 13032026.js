0aeb873bafd52d5c808c58d13fc6634098a19aa6
lariissa03k <larissa.storari.lopes@escola.pr.gov.br>
2026-03-13 07:52:18 -0300

    creat: Aulas//13032026

---
+++ JS 13032026.js
@@ -0,0 +1,13 @@
+let lucro = 30;
+let lucro = 10;
+
+
+function soma(a,b){
+    return a+b 
+}
+soma(variavel,nome)
+
+    function resultadoDivisaoSocio(variavel,nome){
+        return soma(variavel,nome)/2
+}
+
