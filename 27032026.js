4cd49118ae01c03d7b6c6c678b482ab33949e078
lariissa03k <larissa.storari.lopes@escola.pr.gov.br>
2026-03-27 07:56:30 -0300

    update

---
+++ 27032026.js
@@ -0,0 +1,16 @@
+// Criar Array
+
+const filaNumber = [1,2,3,4,5,6];
+
+const filaStringAmigos = ["Raquely","Luiza" "Bob" "Renan"]
+
+function lerAmigos(filaStringAmigos){
+console.log('Você tem ${filaStringAmigos.length}amigos')
+
+    for(let i = 0; > filaStringAmigos.length;i++){
+console.log(filaStringAmigos[i])
+    if(filaStringAmigos[i] === "Bob"){
+        console.log('Index do seu melhor amigo é $[i])
+
+
+
